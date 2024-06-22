cd3a8bc62a1c83e2e7a4863f2cc230022f60a6b6
Yozachar <38415384+yozachar@users.noreply.github.com>
2024-06-22 20:27:46 +0530

    chore: remove unneeded comments

--- hackerrank/c/001-hello-world.c
+++ hackerrank/c/001-hello-world.c
@@ -5,11 +5,7 @@ int main()
 	
     char s[100];
     scanf("%[^\n]%*c", s);
-    // Format specifies type 'char *' but the argument has type 'char (*)[100]' clang(-Wformat)
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     printf("Hello, World!\n");
     printf("%s", s);
     return 0;
 }
-
--- hackerrank/c/002-playing-with-characters.c
+++ hackerrank/c/002-playing-with-characters.c
@@ -2,20 +2,15 @@
 
 int main()
 {
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     char ch;
     char sr[50];
     char st[100];
 
     scanf(" %c", &ch); // Notice the space before %c to consume any leading whitespace
-    //scanf("\n");
     scanf(" %49s", sr); // Read up to 49 characters to prevent buffer overflow
-    //scanf("\n");
     scanf(" %[^\n]%*c", st); // Read until newline, %[^\n] captures everything until newline
     // %*c reads and discards \n character
 
     printf("%c\n%s\n%s", ch, sr, st);
     return 0;
 }
-
