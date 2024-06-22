113db1ed4e36fe7923f87b4b3c4ab08c5162ab0b
Yozachar <38415384+yozachar@users.noreply.github.com>
2024-06-22 21:19:44 +0530

    feat: solved for pointers

---
+++ hackerrank/c/005-pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int s = *a + *b;
+    int d = (*a > *b) ? (*a - *b) : (*b - *a);
+    *a = s; *b = d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
