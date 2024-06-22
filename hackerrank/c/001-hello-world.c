995378addf32bf82e539e214860244e32b891cc4
Yozachar <38415384+yozachar@users.noreply.github.com>
2024-06-22 12:48:53 +0530

    feat: init c @ hackerrank

---
+++ hackerrank/c/001-hello-world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s); // what does format '%[^ mean?
+    // Format specifies type 'char *' but the argument has type 'char (*)[100]' clang(-Wformat)
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    printf("Hello, World!\n");
+    printf("%s", s);
+    return 0;
+}
