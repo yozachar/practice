323c25ac4b142b14a72dafe463def93b0a5620f6
Yozachar <38415384+yozachar@users.noreply.github.com>
2024-07-01 22:28:30 +0530

    feat: solved for 1d arrays

---
+++ hackerrank/c/011-1d-arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  int *arr = (int *)malloc(n * sizeof(int));
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+  printf("%d", sum);
+  
+  free(arr);
+
+  return 0;
+}
